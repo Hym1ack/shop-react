16a4ca978630a83bf16f032c51f532fe857ed248
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-10-12 15:32:35 +0300

    Изменил роуты

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import { Route, Routes } from "react-router-dom";
 import ShopProducts from "./pages/ShopProducts/ShopProducts";
 import Layout from "./pages/Layout/Layout";
-import ProductContainer from "./components/Product/ProductContainer";
-import { localeData } from "./database/localeData";
 import CartPage from "./pages/Cart/CartPage";
 import NotFound from "./pages/NotFound/NotFound";
 import Home from "./pages/Home/Home";
+import ProductPage from "./pages/Product/ProductPage";
+import ProductContainer from "./components/Product/ProductContainer";
 
 function App() {
-  const routesProducts = localeData.map((obj) =>
-    obj.items.map((link) => (
-      <Route path={link.to} element={<ProductContainer />} />
-    ))
-  );
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<ShopProducts />}>
-          {routesProducts}
+          <Route path=":category" element={<ProductContainer />} />
         </Route>
+        <Route path="product/:id" element={<ProductPage />} />
         <Route path="cart" element={<CartPage />} />
-
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
