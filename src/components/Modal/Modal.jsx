8a0156a33bb4976d7dc04d76448d9a07b0212a10
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-10-12 17:29:55 +0300

    Сделал возможность закрыть модальное окно по нажатию на ESC, исправил затемнение при ресайзе окна

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,26 @@
 import { createPortal } from "react-dom";
+import { useEffect } from "react";
 import s from "./Modal.module.css";
 import modalClose from "../../assets/images/popups/modalExit.svg";
 
 const modalRootElem = document.querySelector("#modal");
 
 function Modal({ children, open, onClose }) {
+  useEffect(() => {
+    const close = (e) => {
+      if (e.keyCode === 27) {
+        onClose();
+      }
+    };
+
+    window.addEventListener("keydown", close);
+    return () => window.removeEventListener("keydown", close);
+  }, [onClose]);
+
   if (!open) return null;
 
   return createPortal(
-    <div
-      className={open ? `${s.modalContainer} ${s.active}` : s.modalContainer}
-    >
+    <div className={s.modalContainer}>
       {/* eslint-disable-next-line jsx-a11y/control-has-associated-label,jsx-a11y/click-events-have-key-events */}
       <div
         className={s.modalOverlay}
