afc4d8df1428a53764051bb49491089fa9381f39
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-10-12 17:26:10 +0300

    Отступы для слайдера

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -4,7 +4,7 @@ import s from "./CartPage.module.css";
 import Basket from "../../components/Basket/Basket";
 import Delivery from "../../components/Basket/Delivery";
 import Slider from "../../components/Slider/Slider";
-import Product from "../../components/Product/Product";
+import ProductCard from "../../components/Product/ProductCard/ProductCard";
 import { addToCart } from "../../redux/cartSlice";
 
 function CartPage() {
@@ -17,10 +17,15 @@ function CartPage() {
         <Basket />
         <Delivery />
       </div>
-      <Slider title="Рекомендации для вас" type={1} slidesPerView={4}>
+      <Slider
+        title="Рекомендации для вас"
+        type={1}
+        slidesPerView={4}
+        titleStyle={{ marginBottom: "77px" }}
+      >
         {elements.map((product) => (
           <SwiperSlide key={product.id}>
-            <Product
+            <ProductCard
               product={product}
               dispatch={dispatch}
               addToCart={addToCart}
