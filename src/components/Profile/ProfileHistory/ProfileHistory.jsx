d0abde4d02161addcdb0d0b7f28335858a8e80ab
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-10-17 14:30:15 +0300

    Страница с историей пользователя

---
+++ src/components/Profile/ProfileHistory/ProfileHistory.jsx
@@ -0,0 +1,25 @@
+import s from "./ProfileHistory.module.css";
+import OrderCard from "../../OrderCard/OrderCard";
+import { ordersData } from "../../../database/localeData";
+
+function ProfileHistory() {
+  return (
+    <div className={s.history}>
+      <p className={s.warning}>
+        Если не понравился вкус или качество продукта — поможем по всем
+        вопросам. Поддержка 8 800 999 99 99
+      </p>
+      {ordersData.length !== 0 ? (
+        <div className={s.orders}>
+          {ordersData.map((order) => (
+            <OrderCard order={order} key={order.orderId} />
+          ))}
+        </div>
+      ) : (
+        <p>История отсутсвует</p>
+      )}
+    </div>
+  );
+}
+
+export default ProfileHistory;
