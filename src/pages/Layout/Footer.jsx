3b196b2ea60e237983609ca4e86e661b81da557e
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-10-18 23:22:05 +0300

    Адаптив для футера

--- src/pages/Layout/Footer.jsx
+++ src/pages/Layout/Footer.jsx
@@ -1,87 +1,89 @@
 import s from "./Footer.module.css";
-import cards from "../../assets/images/footer/footerCards.png";
 import phoneImg from "../../assets/images/footer/phone.svg";
 import locationImg from "../../assets/images/footer/location-image.svg";
 import instImg from "../../assets/images/footer/insta-logo.svg";
 import emailImg from "../../assets/images/footer/email-logo.svg";
+import { ReactComponent as VisaMastercard } from "../../assets/images/footer/VisaMastercard.svg";
+import { AppLink } from "../../components/UiKit/AppLink";
+import SubscribeNews from "../../components/SubscribeNews/SubscribeNews";
 
 function Footer() {
+  const footerElements = [
+    {
+      title: "Ильинский онлайн",
+      class: s.online,
+      links: [
+        { to: "bakery", label: "Кулинария" },
+        { to: "water", label: "Супермаркет" },
+        { to: "dumplings", label: "Заморозка" },
+        { to: "beauty", label: "Другое" },
+      ],
+    },
+    {
+      title: "Ильинский клуб",
+      class: s.club,
+      links: [
+        { to: "bakery", label: "Акции" },
+        { to: "water", label: "Доставка и оплата" },
+        { to: "dumplings", label: "Программа лояльности" },
+        { to: "beauty", label: "Политика конфиденциальности" },
+        { to: "beauty", label: "Вакансии" },
+      ],
+    },
+    {
+      title: "",
+      phone: true,
+      class: s.feedback,
+      links: [
+        { to: "bakery", label: "Адреса магазинов", img: locationImg },
+        { to: "water", label: "Следите за нами", img: instImg },
+        { to: "dumplings", label: "Обратная связь", img: emailImg },
+      ],
+    },
+  ];
+
   return (
     <footer className={s.footer}>
       <div className="container">
         <div className={s.information}>
-          <div className={s.infoColumn}>
-            <h6 className={s.infoTitle}>Ильинский онлайн</h6>
-            <ul className={s.links}>
-              <li className={s.link}>Кулинария</li>
-              <li className={s.link}>Супермаркет</li>
-              <li className={s.link}>Заморозка</li>
-              <li className={s.link}>Другое</li>
-            </ul>
-          </div>
-          <div className={s.infoColumn}>
-            <h6 className={s.infoTitle}>Ильинский клуб</h6>
-            <ul className={s.links}>
-              <li className={s.link}>Акции</li>
-              <li className={s.link}>Доставка и оплата</li>
-              <li className={s.link}>Программа лояльности</li>
-              <li className={s.link}>Политика конфиденциальности</li>
-              <li className={s.link}>Вакансии</li>
-            </ul>
-          </div>
-          <div className={s.infoColumn}>
-            <div className={s.phone}>
-              <p className={s.phoneNumber}>
-                <img className={s.phoneImg} src={phoneImg} alt="phone" />
-                <span>+7 (333) 33-33-33</span>
-              </p>
-              <p className={s.phoneDescr}>Ежедневно c 09:00 до 21:00</p>
+          {footerElements.map((element) => (
+            <div className={element.class}>
+              {element.title && (
+                <h6 className={s.infoTitle}>{element.title}</h6>
+              )}
+              {element.phone && (
+                <div className={s.phone}>
+                  <p className={s.phoneNumber}>
+                    <img className={s.phoneImg} src={phoneImg} alt="phone" />
+                    <span>+7 (333) 33-33-33</span>
+                  </p>
+                  <p className={s.phoneDescr}>Ежедневно c 09:00 до 21:00</p>
+                </div>
+              )}
+              <ul className={s.links}>
+                {element.links.map((link) => (
+                  <AppLink className={s.link} to={link.to}>
+                    {link.img && (
+                      <img
+                        className={s.linkImg}
+                        src={link.img}
+                        alt="imgBefore"
+                      />
+                    )}
+                    {link.label}
+                  </AppLink>
+                ))}
+              </ul>
             </div>
-            <ul className={s.links}>
-              <li className={s.link}>
-                <a className={s.linkContainer} href="/">
-                  <img className={s.linkImg} src={locationImg} alt="location" />
-                  <p>Адреса магазинов</p>
-                </a>
-              </li>
-              <li className={s.link}>
-                <a className={s.linkContainer} href="/">
-                  <img className={s.linkImg} src={instImg} alt="instagram" />
-                  <p>Следите за нами</p>
-                </a>
-              </li>
-              <li className={s.link}>
-                <a className={s.linkContainer} href="/">
-                  <img className={s.linkImg} src={emailImg} alt="email" />
-                  <p>Обратная связь</p>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className={s.infoColumn}>
-            <fieldset>
-              <legend className={s.subscribeText}>
-                Подпишитесь на вкусные и полезые новости
-              </legend>
-              <div>
-                <input className={s.subscribeInput} type="email" />
-                <button className={s.subscribeButton} type="submit">
-                  Подписаться
-                </button>
-              </div>
-              <div className={s.politics}>
-                <input type="checkbox" id="checkbox" />
-                <label className={s.politicsLink} htmlFor="checkbox">
-                  Согласен с политикой конфиденциальности
-                </label>
-              </div>
-            </fieldset>
+          ))}
+          <div className={s.subscribe}>
+            <SubscribeNews />
           </div>
         </div>
         <div className={s.footerBottom}>
-          <p>© 2022 Ильинский онлайн — доставкатоваров и продуктов на дом</p>
+          <p>© 2022 Ильинский онлайн — доставка товаров и продуктов на дом</p>
           <p>Информация на сайте не является публичной офертой</p>
-          <img src={cards} alt="visa & mastercard" />
+          <VisaMastercard />
         </div>
       </div>
     </footer>
