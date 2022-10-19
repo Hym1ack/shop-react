dd06c7cad5148d5613fd7b8ea12500d3d84f93e9
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-10-19 16:28:00 +0300

    Блок корзины

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import s from "./Cart.module.css";
+import cartImage from "../../assets/images/header/shopping-cart.svg";
+
+function Cart() {
+  const { totalAmount, totalQuantity } = useSelector((state) => state.cart);
+
+  const calcQuantityEnding = (quantity) => {
+    const ending = ["товар", "товара", "товаров"];
+
+    if (quantity === 1) return ending[0];
+    if (quantity > 1 && quantity < 5) return ending[1];
+
+    return `${quantity} ${ending[2]}`;
+  };
+
+  return (
+    <Link to="cart" className={s.shopCart}>
+      <img src={cartImage} alt="cart" className={s.cartImage} />
+      {totalQuantity === 0 ? (
+        <span className={s.cartDefault}>Корзина</span>
+      ) : (
+        <p className={s.cartNotEmpty}>
+          {`${totalAmount} ₽`}
+          <br />
+          <span className={s.cartQuantity}>
+            {calcQuantityEnding(totalQuantity)}
+          </span>
+        </p>
+      )}
+    </Link>
+  );
+}
+
+export default Cart;
