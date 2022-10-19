8e7676ebdd7c479056a2d363b3e4ae6b3dc2409d
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-10-19 16:24:09 +0300

    Адаптив для header

--- src/pages/Layout/Header.jsx
+++ src/pages/Layout/Header.jsx
@@ -1,63 +1,36 @@
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useMediaQuery } from "react-responsive";
 import s from "./Header.module.css";
-import logo from "../../assets/images/header/logo.svg";
-import cartImage from "../../assets/images/header/shopping-cart.svg";
+import { ReactComponent as AppLogo } from "../../assets/images/header/logo.svg";
 import Navigation from "../../components/Navigation/Navigation";
 import ProfileButton from "../../components/Profile/ProfileButton";
 import { AppLink } from "../../components/UiKit/AppLink";
 import { ReactComponent as FavouriteLogo } from "../../assets/images/header/favourites.svg";
+import Search from "../../components/Search/Search";
+import Cart from "../../components/Cart/Cart";
 
 function Header() {
-  const { totalAmount, totalQuantity } = useSelector((state) => state.cart);
-
-  const calcQuantityEnding = (quantity) => {
-    const ending = ["товар", "товара", "товаров"];
-
-    if (quantity === 1) return ending[0];
-    if (quantity > 1 && quantity < 5) return ending[1];
-
-    return ending[2];
-  };
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
 
   return (
     <header className={s.header}>
       <div className="container">
         <div className={s.headerTop}>
           <Link to="/">
-            <img src={logo} alt="logo" />
+            <AppLogo />
           </Link>
-          <fieldset className={s.searchBlock}>
-            <input className={s.search} placeholder="Начать поиск" />
-            <span className={s.iconSearch} />
-          </fieldset>
-          <div className={s.headerRight}>
-            <AppLink to="profile/favourites">
-              <FavouriteLogo className={s.btnImg} />
-            </AppLink>
-            <ProfileButton />
-
-            <Link to="cart" className={s.shopCart}>
-              <img src={cartImage} alt="cart" className={s.cartImage} />
-              {totalQuantity === 0 ? (
-                <span className={s.cartDefault}>Корзина</span>
-              ) : (
-                <p className={s.cartNotEmpty}>
-                  {`${totalAmount} ₽`}
-                  <br />
-                  <span
-                    className={s.cartQuantity}
-                  >{`${totalQuantity} ${calcQuantityEnding(
-                    totalQuantity
-                  )}`}</span>
-                </p>
-              )}
-            </Link>
-          </div>
+          <Search />
+          {!isMobile && (
+            <div className={s.headerRight}>
+              <AppLink to="profile/favourites">
+                <FavouriteLogo className={s.btnImg} />
+              </AppLink>
+              <ProfileButton />
+              <Cart />
+            </div>
+          )}
         </div>
-        <div>
-          <Navigation />
-        </div>
+        <Navigation />
       </div>
     </header>
   );
