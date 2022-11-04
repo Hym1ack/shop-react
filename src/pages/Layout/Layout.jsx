69458f04cc4030983a6aab078245ca6d4b6507c8
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-11-05 01:32:39 +0300

    Убрал запрос на рек. товары на всех страницах

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,17 +1,8 @@
 import { Outlet } from "react-router-dom";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
 import Header from "./Header";
 import Footer from "./Footer";
-import { fetchRecommendedProducts } from "../../redux/shopSlice";
 
 function Layout() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchRecommendedProducts());
-  }, [dispatch]);
-
   return (
     <div className="app">
       <Header />
