3df2155c244ae79ec43c2c98d226f6571297e45f
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-11-05 01:31:36 +0300

    Вынес ссылки с данными в отдельный файл

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,25 +8,10 @@ import "swiper/css/free-mode";
 // import required modules
 import { FreeMode } from "swiper";
 import s from "./Navigation.module.css";
-
-import marketImage from "../../assets/images/header/header-supermarket.png";
-import cookingImage from "../../assets/images/header/header-cooking.png";
-import freezeImage from "../../assets/images/header/header-freezen.png";
-import anotherImage from "../../assets/images/header/header-another.png";
-import stockImage from "../../assets/images/header/header-stock.png";
-import shopsImage from "../../assets/images/header/header-shops.png";
-import { AppLink } from "../UiKit/AppLink";
+import AppLink from "../UiKit/AppLink";
+import { navigationLinks } from "../../database/navigationLinks";
 
 function Navigation() {
-  const links = [
-    { to: "shop/water", label: "Супермаркет", image: marketImage },
-    { to: "shop/bakery", label: "Кулинария", image: cookingImage },
-    { to: "shop/dumplings", label: "Заморозка", image: freezeImage },
-    { to: "shop/beauty", label: "Другое", image: anotherImage },
-    { to: "discounts", label: "Акции", image: stockImage },
-    { to: "address", label: "Магазины", image: shopsImage },
-  ];
-
   return (
     <nav className={s.list}>
       <Swiper
@@ -48,7 +33,7 @@ function Navigation() {
           },
         }}
       >
-        {links.map((link) => (
+        {navigationLinks.map((link) => (
           <SwiperSlide key={link.to} className={s.slide}>
             <AppLink
               className={s.item}
