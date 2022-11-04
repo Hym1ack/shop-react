9de39a29f7219859a5b759e275b0fea4185ca788
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-11-05 01:33:15 +0300

    Перенёс ссылки с футера в отдельный файл

--- src/pages/Layout/Footer.jsx
+++ src/pages/Layout/Footer.jsx
@@ -1,52 +1,16 @@
 import s from "./Footer.module.css";
 import phoneImg from "../../assets/images/footer/phone.svg";
-import locationImg from "../../assets/images/footer/location-image.svg";
-import instImg from "../../assets/images/footer/insta-logo.svg";
-import emailImg from "../../assets/images/footer/email-logo.svg";
 import { ReactComponent as VisaMastercard } from "../../assets/images/footer/VisaMastercard.svg";
-import { AppLink } from "../../components/UiKit/AppLink";
+import AppLink from "../../components/UiKit/AppLink";
 import SubscribeNews from "../../components/SubscribeNews/SubscribeNews";
+import { footerLinks } from "../../database/footerData";
 
 function Footer() {
-  const footerElements = [
-    {
-      title: "Ильинский онлайн",
-      class: s.online,
-      links: [
-        { id: 1, to: "bakery", label: "Кулинария" },
-        { id: 2, to: "water", label: "Супермаркет" },
-        { id: 3, to: "dumplings", label: "Заморозка" },
-        { id: 4, to: "beauty", label: "Другое" },
-      ],
-    },
-    {
-      title: "Ильинский клуб",
-      class: s.club,
-      links: [
-        { id: 1, to: "bakery", label: "Акции" },
-        { id: 2, to: "water", label: "Доставка и оплата" },
-        { id: 3, to: "dumplings", label: "Программа лояльности" },
-        { id: 4, to: "beauty", label: "Политика конфиденциальности" },
-        { id: 5, to: "beauty", label: "Вакансии" },
-      ],
-    },
-    {
-      title: "",
-      phone: true,
-      class: s.feedback,
-      links: [
-        { id: 1, to: "bakery", label: "Адреса магазинов", img: locationImg },
-        { id: 2, to: "water", label: "Следите за нами", img: instImg },
-        { id: 3, to: "dumplings", label: "Обратная связь", img: emailImg },
-      ],
-    },
-  ];
-
   return (
     <footer className={s.footer}>
       <div className="container">
         <div className={s.information}>
-          {footerElements.map((element) => (
+          {footerLinks.map((element) => (
             <div className={element.class} key={element.class}>
               {element.title && (
                 <h6 className={s.infoTitle}>{element.title}</h6>
