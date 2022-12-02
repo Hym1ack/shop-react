d17270d3e8acab4bb9e4bf89ce823628ade4e663
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-12-02 16:35:11 +0300

    Если юзер не авторизован то модалка с предложением залогиниться

--- src/components/UiKit/Favourite.jsx
+++ src/components/UiKit/Favourite.jsx
@@ -1,13 +1,16 @@
 import { useDispatch } from "react-redux";
+import { useState } from "react";
 import { ReactComponent as HeartCircle } from "../../assets/images/heartCircle.svg";
 import { ReactComponent as Heart } from "../../assets/images/heart.svg";
 import s from "./Favourite.module.css";
 import { useAuth } from "../../hooks/useAuth";
 import { toggleFavourite } from "../../redux/userSlice";
+import ModalAuth from "../ModalAuth/ModalAuth";
 
 function Favourite({ circle = false, id }) {
   const { isAuth, favoritesProductsId } = useAuth();
   const dispatch = useDispatch();
+  const [modalOpen, setModalOpen] = useState(false);
 
   const favorited = !!favoritesProductsId.includes(id);
 
@@ -16,15 +19,28 @@ function Favourite({ circle = false, id }) {
   const addToFavourites = () => {
     if (isAuth) {
       dispatch(toggleFavourite(id));
+    } else {
+      setModalOpen(true);
     }
   };
 
   return (
-    <button type="button" className={s.button} onClick={addToFavourites}>
-      {circle && <HeartCircle className={classes} />}
+    <>
+      <button type="button" className={s.button} onClick={addToFavourites}>
+        {circle ? (
+          <HeartCircle className={classes} />
+        ) : (
+          <Heart className={classes} />
+        )}
+      </button>
 
-      {!circle && <Heart className={classes} />}
-    </button>
+      {modalOpen && (
+        <ModalAuth
+          modalOpen={modalOpen}
+          setModalOpen={() => setModalOpen(false)}
+        />
+      )}
+    </>
   );
 }
 
