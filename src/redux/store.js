e2afadd27a2dec2e4e6358daffa34ee446b03900
Pavlov Kirill <pavlovkirill322@gmail.com>
2022-12-08 00:48:04 +0300

    Подключил persist для сохранения состояния в localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,42 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+
+import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistReducer,
+  persistStore,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
 import shopReducer from "./shopSlice";
 import cartReducer from "./cartSlice";
 import userReducer from "./userSlice";
 
-export const store = configureStore({
-  reducer: {
-    shop: shopReducer,
-    user: userReducer,
-    cart: cartReducer,
-  },
+const rootReducer = combineReducers({
+  shop: shopReducer,
+  user: userReducer,
+  cart: cartReducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+export default store;
