74198d7c16018bee4418d23af0a625bec0d5617e
Pavlov Kirill <pavlovkirill322@gmail.com>
2023-01-22 00:34:38 +0300

    Исправил то что, забыл добавить env для firebase

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBy6wi6jR8k2CjEsgnzjeuZK2JoXjEQXek",
-  authDomain: "shopreact-14a92.firebaseapp.com",
-  projectId: "shopreact-14a92",
-  storageBucket: "shopreact-14a92.appspot.com",
-  messagingSenderId: "169304901223",
-  appId: "1:169304901223:web:2120132dd078fa80c276c2",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
 const app = initializeApp(firebaseConfig);
 const firestoreDatabase = getFirestore(app);
 const auth = getAuth(app);
